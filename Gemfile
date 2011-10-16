fa8251d566255ebb6f2939c1895923d06f6c9af6
stonehz <stonehz@gmail.com>
2011-10-16 14:23:32 +0100

    new Rails app generated by Rails Apps Composer gem

---
+++ Gemfile
@@ -0,0 +1,19 @@
+require 'rbconfig'
+HOST_OS = Config::CONFIG['host_os']
+source 'http://rubygems.org'
+gem 'rails', '3.1.1'
+gem 'sqlite3'
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+group :test do
+  
+  gem 'turn', :require => false
+end
+if HOST_OS =~ /linux/i
+  gem 'therubyracer', '>= 0.8.2'
+end
+gem "devise", ">= 1.4.7"
+gem "rails-footnotes", ">= 3.7", :group => :development
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '9205009dcb9656aa5f885b0b564fea3a5beaaaca2985c6498126951f23cfea129add4aa73a4b9957b5457c3d677cf79cd3770dc463cdc742950dcbf971caf721'
